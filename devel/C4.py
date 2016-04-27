c39b7e1e9f092f0418989d3fb9a0b171ab0c15d6
Richard Tran <rtran4@mail.valenciacollege.edu>
2016-04-27 15:14:38 -0400

    C4
    Having a hard time trying to get a winner for my program.

---
+++ devel/C4.py
@@ -0,0 +1,67 @@
+from random import randint
+
+
+class connectFour:
+    def __init__ (self, rows = 7, col = 8, fourInRow = 4):
+        self.col = col
+        self.win = fourInRow
+        self.rows = rows
+        self.board = [['.'] * rows for _ in range(col)]
+    
+    def insert(self, column, color):
+        c = self.board[column]
+        if c[0] != '.':
+            raise Exception('Column is full')
+        i = -1
+        while c[i] != '.':
+            i -= 1
+        c[i] = color
+        self.checkWinner
+
+    def checkWinner(self):
+        print('Checking if ' + player + ' is a winner...')
+        w = self.Winner()
+        if w:
+            self.printBoard()
+            raise Exception(w + ' Winner!')
+        if (checkWinner('R')):
+            print('RED WINS!')
+        elif (checkWinner('Y')):
+            print('YELLOW WINS!')
+
+        
+    def printBoard(self):
+        print(' '.join(map(str, range(self.col))))
+        for y in range(self.rows):
+            print(' '.join(str(self.board[x][y]) for x in range(self.col)))
+
+    def startGame(Player, board):
+        turn = Player
+        for i in range(9):
+            four.printBoard()
+            move = input()
+            if (checkWinner(board, 'R')):
+                print ('RED WINS!')
+                break
+            elif (checkWinner(board, 'Y')):
+                print ('YELLOW WINS!')
+                break
+
+             
+if __name__ == '__main__':
+    four = connectFour()
+    turn = 'R'
+    while True:
+        four.printBoard()
+        row = input('{}\'s turn: '.format('Red' if turn == 'R' else 'Yellow'))
+        four.insert(int(row), turn)
+        turn = 'Y' if turn == 'R' else 'R'
+        
+
+      
+    
+
+
+
+    
+
--- devel/README.md
+++ devel/README.md
@@ -1,3 +1,3 @@
 # Instructions for Running my Project
 
-Replace this text with the instructions your classmates will need to run your project. If your code needs any libraries using `pip` or any other tool, those instructions should be here as well. A complete stranger should be able to follow these instructions, so don't be afraid to include videos, screenshots, diagrams, text, whatever helps!
+It's pretty straight forward. Just open the .py and tun the program. I am having a really hard time trying to get the program to find a winner, so it will keep running. It's a connect four board and it takes turns between Red and Yellow. It's not against an AI so you're both the Red and the Yellow. 
